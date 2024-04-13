b5f8370617c54b9fd6c27b13404c9bda5f5bac1b
Nikita Barmin <barminnikita22077@mail.ru>
2024-04-14 00:34:39 +0300

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import Navbar from './Components/Navbar';
 import './index.css';
 
 
 const Main = () => {
+  useEffect(() => {
+    //как делать пост-запрос
+    /*fetch("http://localhost:3500/register",{
+      method:"POST",
+      body: JSON.stringify({
+        user:"Nikita",
+        pwd: "1234",
+      }),
+      headers:{
+        "Content-type":"application/json"
+      }
 
+    }).then(response => response.json()).then(json => console.log(json)).catch(error=>console.log(error));*/
+    fetch("http://localhost:3500/login",{
+      method:"POST",
+      body: JSON.stringify({
+        user:"Nikita",
+        pwd: "1234",
+      }),
+      headers:{
+        "Content-type":"application/json"
+      }
+
+    }).then(response => response.json()).then(json => console.log(json)).catch(error=>console.log(error));
+
+  },[]);
 
   return (
     <Navbar />
