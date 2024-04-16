9d6449bfdd91840e8698840df3cdb3fb513e66fc
neoplasmes <zverkovegor@gmail.com>
2024-04-16 18:26:03 +0300

    dsgfsdg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React,{useEffect} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import Navbar from './Components/Navbar';
 import './index.css';
+
 
 
 const Main = () => {
@@ -18,22 +20,15 @@ const Main = () => {
       }
 
     }).then(response => response.json()).then(json => console.log(json)).catch(error=>console.log(error));*/
-    fetch("http://localhost:3500/login",{
-      method:"POST",
-      body: JSON.stringify({
-        user:"Nikita",
-        pwd: "1234",
-      }),
-      headers:{
-        "Content-type":"application/json"
-      }
-
-    }).then(response => response.json()).then(json => console.log(json)).catch(error=>console.log(error));
 
   },[]);
 
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
