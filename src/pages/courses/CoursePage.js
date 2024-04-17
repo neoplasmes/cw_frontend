7fe978bbcc637b79e620b71ffe1fe85f86a8ce6e
neoplasmes <zverkovegor@gmail.com>
2024-04-17 03:23:13 +0300

    fffff

--- src/pages/courses/CoursePage.js
+++ src/pages/courses/CoursePage.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from '../../axios/axios';
+
+
 
 export const CoursePage = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(()=>{
+    const controller = new AbortController();
+
+    const getCourses = async () => {
+      const response = await axios.get("/courses", {
+        signal: controller.signal
+      });
+      
+      console.log(response);
+      if (response.status === 200){
+        setCourses(response.data);
+      }
+
+      return () => {
+        controller.abort();
+      }
+    }
+
+
+    getCourses();
+  },[])
+
   return (
-    <div>
-        CoursePage
+    <div className='courses-wrapper margin-global'>
+        {courses.map((c) => {
+          return (<div key={c.id}>{c.course_name}</div>);
+        })}
     </div>
   )
 }
